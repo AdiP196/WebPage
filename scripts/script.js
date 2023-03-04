00dd731c297c943dfb2c56864d1e4537422d6f35
AdiP196 <adipopa229@gmail.com>
2023-03-04 21:50:24 +0200

    remove script.js

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const ToggleButton = document.getElementsById('toggleButton');
-const naviList = document.getElementsById('nav_list');
-
-ToggleButton.addEventListener('click', () => {
-    naviList.classList.toggle('active');
-    
-})
